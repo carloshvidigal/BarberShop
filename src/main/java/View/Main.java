28425d4e2c0e560a9502f5276e0806231b9a09d3
carloshvidigal <chmovi@hotmail.com>
2025-02-19 13:46:40 -0300

    Criação do client e classe de teste main

--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package View;
-
+import Model.Service;
 /**
  *
  * @author chmov
  */
 public class Main {
+    
+    public static void main(String[] args) {
+        
+        Service service = new Service(1, "beard", 30);
+        
+        System.out.println(service.getDescription());
+        System.out.println(service.getValue());
+        
+        
+        
+        
+    }
     
 }
